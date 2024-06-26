86d090139d1ee95a36c13ff7ab5aab61bc58638a
yeogeru <yjw0835@naver.com>
2024-06-26 22:51:24 +0900

    fix: build output fix

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -12,7 +12,7 @@ module.exports = merge(config, {
     path: path.resolve(__dirname, 'docs'),
     filename: '[name].[contenthash].js',
     clean: true,
-    publicPath: '/',
+    publicPath: '/portfolio/',
   },
   plugins: [
     new CleanWebpackPlugin(),
