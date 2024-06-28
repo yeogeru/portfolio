95eb18d93a39d44be24a5f9704a5aa4e4d50659c
yeogeru <yjw0835@naver.com>
2024-06-28 23:50:19 +0900

    fix: build output ifx

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,25 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className="bg-white dark:bg-zinc-900">
-      <div className="w-full h-14 fixed bg-slate-500">
-        <div className="bg-slate-600 w-3/4 max-sm:w-full h-full mx-auto">
+    <div className="min-h-[100vh] bg-white dark:bg-zinc-900">
+      <div className="w-full h-16 fixed bg-slate-500">
+        <div className='w-full max-w-[120rem] h-full flex items-center bg-slate-600 mx-auto px-4'>
+          {/* 
+              뭘 넣는게 좋을까?
+              언어설정, 다크모드, pdf저장?
+          */}
           <p>이곳은 바로가기가 들어갈 자리입니다.</p>
+          <p className='flex-1'></p>
+          <p className=''>그외</p>
         </div>
       </div>
-      <div className="bg-gray-400 w-3/4 max-sm:w-full h-full pt-14 mx-auto">
+      <div className="w-full max-w-[120rem] h-full bg-gray-400 pt-16 mx-auto">
         <div className="w-full h-96 bg-gray-200 dark:bg-zinc-800 dark:text-white">
           <p>이곳은 소개 글이 들어갈 자리입니다.</p>
-        </div>
-        <div className='w-full h-96 bg-gray-400'>
-          <p>이곳은 다른 컨텐츠가 들어갈 자리입니다.</p>
+          {/* 
+              아니 뭐넣냐고
+
+          */}
         </div>
       </div>
     </div>
